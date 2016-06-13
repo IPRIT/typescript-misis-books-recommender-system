fc4ad73c264bc0d7c0884be0cc614af73d90c5e6
Alex <ipritoflex@yandex.ru>
2016-06-13 18:25:16 +0300

    Removed unnecessary files

--- src/config.js
+++
@@ -1,5 +0,0 @@
-exports.CONF = {
-    port: 8081,
-    ip: '10.135.1.53'
-};
-//# sourceMappingURL=config.js.map
--- src/internal/user/auth/index.js
+++
@@ -1,2 +0,0 @@
-module.exports = require('./auth');
-//# sourceMappingURL=index.js.map
